01a633b21b17bf0bc8c6ac8b5ab0d85a266aa7c5
JorgeFabian315 <jfabi@FABIAN>
2024-11-11 21:31:58 -0600

    ,

--- ReticulaPWA/wwwroot/scripts/perfil.js
+++ ReticulaPWA/wwwroot/scripts/perfil.js
@@ -21,18 +21,19 @@ const mapeoPerfil = () => {
 
 
     document.getElementById("perfil__carrera").textContent = perfilDto.carrera;
-    document.getElementById("perfil__planEstudios").textContent =
-        perfilDto.planDeEstudios;
-    document.getElementById("perfil__especialidad").textContent =
-        perfilDto.especialidad;
-    document.getElementById("perfil__creditos").textContent =
-        perfilDto.creditosTotales;
+    document.getElementById("perfil__planEstudios").textContent = perfilDto.planDeEstudios;
+    document.getElementById("perfil__especialidad").textContent = perfilDto.especialidad;
+    document.getElementById("perfil__creditos").textContent = perfilDto.creditosTotales;
     document.getElementById("perfil__vigencia").textContent = perfilDto.vigencia;
     document.getElementById("perfil__vigencia").textContent = perfilDto.vigencia;
-    document.getElementById("perfil__periodoActual").textContent =
-        perfilDto.periodoActualUltimo;
-    document.getElementById("perfil__numConvalidados").textContent =
-        perfilDto.periodosConvalidos;
+    document.getElementById("perfil__periodoActual").textContent = perfilDto.periodoActualUltimo;
+    document.getElementById("perfil__numConvalidados").textContent = perfilDto.periodosConvalidos;
+    document.getElementById("perfil__curp").textContent = perfilDto.curp;
+    document.getElementById("perfil__fechaNacimiento").textContent = perfilDto.fechaNacimiento;
+    document.getElementById("perfil__calle").textContent = perfilDto.calle;
+    document.getElementById("perfil__colonia").textContent = perfilDto.colonia;
+    document.getElementById("perfil__ciudad").textContent = perfilDto.ciudad;
+    document.getElementById("perfil__correo").textContent = perfilDto.correo;
 };
 
 window.addEventListener("DOMContentLoaded", mapeoPerfil);
